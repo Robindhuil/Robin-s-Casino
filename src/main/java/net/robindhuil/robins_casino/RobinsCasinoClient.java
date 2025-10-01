4d101b04369982625fa47e88a99242bd585b9c20
Robinnoss27 <robinnoss27@gmail.com>
2025-10-01 02:15:11 +0200

    Project setup

---
+++ src/main/java/net/robindhuil/robins_casino/RobinsCasinoClient.java
@@ -0,0 +1,10 @@
+package net.robindhuil.robins_casino;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RobinsCasinoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/robindhuil/robins_casino/RobinsCasinoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.robindhuil.robins_casino;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RobinsCasinoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
