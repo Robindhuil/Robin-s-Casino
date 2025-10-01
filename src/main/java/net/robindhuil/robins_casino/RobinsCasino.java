13a08f177f13fa0b188d7d7fbd48c74ea964d79e
Robindhuil <robogasso@gmail.com>
2025-10-01 02:15:11 +0200

    Project setup

---
+++ src/main/java/net/robindhuil/robins_casino/RobinsCasino.java
@@ -0,0 +1,16 @@
+package net.robindhuil.robins_casino;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RobinsCasino implements ModInitializer {
+	public static final String MOD_ID = "robins-casino";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
